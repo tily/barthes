b01ffe9341f5f0df4ca6eea6f54578817d4ec2fb
tily <tidnlyam@gmail.com>
2014-11-02 19:09:26 +0900

    update Barthes::Reporter::JunitXml

--- lib/barthes/reporter/junit_xml.rb
+++ lib/barthes/reporter/junit_xml.rb
@@ -2,6 +2,24 @@
 module Barthes
 	class Reporter
 		class JunitXml
+			def after_run(features)
+				features.each do |feature|
+				end
+			end
+
+			def walk_feature
+			end
+
+			def render_testcase(xml, action)
+				xml.testcase do
+					xml.stdout
+					xml.failure
+					xml.skipped
+					xml.error
+				end
+			end
 		end
 	end
 end
+
+__END__
