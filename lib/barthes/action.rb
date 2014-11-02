1209f4670f5651a765c1412676fd5a1a2b4c71d6
tily <tidnlyam@gmail.com>
2014-11-02 18:49:16 +0900

    update Barthes::Action class

--- lib/barthes/action.rb
+++ lib/barthes/action.rb
@@ -2,35 +2,67 @@ require 'barthes/cache'
 
 module Barthes
 	class Action
-		def initialize(env)
+		def initialize(env, options)
+			@options = options
 			@env = env.dup
 			client_class = Object.const_get(@env['client_class'])
 			@client = client_class.new(env)
 		end
 
+		def indent(size, string)
+			string.split("\n").map {|line| "\t" * size + "#{line}\n" }
+		end
+
 		def action(action)
 			@env.update(action['environment']) if action['environment']
 			params = evaluate_params(action['params'])
-			response = @client.action(params)
-			p "params: #{params}"
-			p "response: #{response}"
+
 			if action['expectations']
-				result = @client.compare(response, action['expectations'])
-			else
-				result = {result: true}
+				if action['max_loop']
+					action['max_loop'].to_i.times do
+						sleep action['sleep'].to_i/1000 if action['sleep']	
+
+						response = @client.action(params)
+						action['expectations'].each do |expectation|
+							result = @client.compare(response, evaluate_params(expectation))
+							expectation.update(result)
+						end
+						if action['expectations'].all? {|e| e['result'] == true }
+							break
+						end
+					end
+				end
 			end
+
+			sleep action['sleep'].to_i/1000 if action['sleep']	
+
+			action['request'] = params
+			action['response'] = response = @client.action(params)
+
+			if action['expectations']
+				action['expectations'].each do |expectation|
+					result = @client.compare(response, evaluate_params(expectation))
+					expectation.update(result)
+				end
+			end
+
 			if cache_config = action['cache']
-				value = @client.cache(cache_config, response)
-				Barthes::Cache.set(config['key'], value)
+				value = @client.extract(cache_config, response)
+				action['cache']['value'] = value
+				Barthes::Cache.set(cache_config['key'], value)
 			end
-			result
+			action
 		end
 
 		def evaluate_params(params)
 			new_params = {}
 			params.each do |k, v|
-				new_v = v.gsub(/\$\{(.+?)\}/) { @env[$1] }
-				new_v = new_v.gsub(/\@\{(.+?)\}/) { Barthes::Cache.get($1) }
+				if v.class == String
+					new_v = v.gsub(/\$\{(.+?)\}/) { @env[$1] }
+					new_v = new_v.gsub(/\@\{(.+?)\}/) { p $1; Barthes::Cache.get($1) }
+				else
+					new_v = v
+				end
 				new_params[k] = new_v
 			end
 			new_params
