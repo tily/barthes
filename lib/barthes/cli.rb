3593e666fea651e5d79f7a42125b609c28e8e9b1
tily <tidnlyam@gmail.com>
2014-11-03 12:47:10 +0900

    tweak option parser

--- lib/barthes/cli.rb
+++ lib/barthes/cli.rb
@@ -7,21 +7,22 @@ require 'slop'
 module Barthes
 	class CLI
 		def self.parse_options
-			opt = Slop.parse!(help: true) do
-			  banner 'Usage: barthes [options]'
+			@opt = Slop.parse!(help: true) do
+			  banner 'Usage: barthes [options] /path/to/some_spec.json'
 			  on 'e', 'env',       'environment file paths',               argument: :optional, as: Array
 			  on 'q', 'quiet',     'not show test details',                argument: :optional
-			  on 'f', 'from',      'test number to start from',            argument: :optional
+			  on 'f', 'from',      'test number to start from',            argument: :optional, as: Integer, default: 1
 			  on 't', 'to',        'test number to stop to',               argument: :optional
 			  on 'l', 'load',      'An optional password',                 argument: :optional
 			  on 'd', 'dryrun',    'not run test but show just structure', argument: :optional
 			  on 'r', 'reporters', 'reporters to use',                     argument: :optional, as: Array
 			end
-			opt.to_hash
+			@opt.to_hash
 		end
 
 		def self.start
 			options = parse_options
+			abort @opt.help if ARGV.empty?
 			Runner.new(options).run(ARGV)
 		end
 	end
