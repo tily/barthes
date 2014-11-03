c54f961e98d823ded67574163065e6bff5960256
tily <tidnlyam@gmail.com>
2014-11-03 16:20:21 +0900

    implement junit xml reporter prototype

--- lib/barthes/reporter/junit_xml.rb
+++ lib/barthes/reporter/junit_xml.rb
@@ -1,21 +1,58 @@
+require 'builder'
+require 'nokogiri'
 
 module Barthes
 	class Reporter
 		class JunitXml
-			def after_run(features)
-				features.each do |feature|
-				end
+			def initialize
+				@xml = Builder::XmlMarkup.new
+				@xml.instruct! :xml, :version=>"1.0", :encoding=>"UTF-8"
 			end
 
-			def walk_feature
+			def after_run(features)
+				result = @xml.testsuites do |xml|
+					features.each do |feature|
+						walk_json(feature)
+					end
+				end
+				puts Nokogiri::XML(result).to_xml(indent: 2)
+			end
+
+			def walk_json(json)
+				case json.first
+				when 'feature', 'scenario'
+					if json.last.class == Array
+						@xml.testsuite(name: json[1], tests: json.last.size) do
+							json.last.each do |child|
+								walk_json(child)
+							end
+						end
+					end
+				when 'action'
+					@xml.testsuite(name: json[1], tests: 1) do
+						@xml.testcase(name: json[1]) do
+							case json.last['status']
+							when 'skipped'
+								@xml.skipped
+							when 'failure'
+							when 'error'
+							end
+							if json.last['status'] != 'skipped' && json.last['request'] && json.last['response']
+								stdout = "request:\n"
+								stdout += "#{JSON.pretty_generate(json.last['request'])}\n"
+								stdout += "response:\n"
+								stdout += "#{JSON.pretty_generate(json.last['response'])}\n"
+								@xml.tag!(:'system-out', stdout)
+							end
+						end
+					end
+				else
+					puts json
+				end
 			end
 
 			def render_testcase(xml, action)
 				xml.testcase do
-					xml.stdout
-					xml.failure
-					xml.skipped
-					xml.error
 				end
 			end
 		end
