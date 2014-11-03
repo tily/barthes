808760e0022e10236fc4b3de185accbdba52ce85
tily <tidnlyam@gmail.com>
2014-11-03 12:21:42 +0900

    use slop instead of thor

--- barthes.gemspec
+++ barthes.gemspec
@@ -21,7 +21,7 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "rake"
 
-  spec.add_dependency "thor"
   spec.add_dependency "json"
   spec.add_dependency "erubis"
+  spec.add_dependency "slop"
 end
--- lib/barthes/cli.rb
+++ lib/barthes/cli.rb
@@ -2,17 +2,27 @@ require 'barthes/runner'
 require 'barthes/reporter/default'
 require 'json'
 require 'thor'
+require 'slop'
 
 module Barthes
-	class CLI < Thor
-		desc 'execute', 'execute tests from json files'
-		option :environment, :type => :string, :aliases => :e 
-		option :verbose, :type => :boolean, :aliases => :v
-		option :dryrun, :type => :boolean, :aliases => :d
-		option :from, :type => :numeric, :aliases => :f, :default => 1
-		option :to, :type => :numeric, :aliases => :t
-		def exec(*paths)
-			Runner.new(options).run(paths)
+	class CLI
+		def self.parse_options
+			opt = Slop.parse!(help: true) do
+			  banner 'Usage: barthes [options]'
+			  on 'e', 'env',       'environment file paths',               argument: :optional, as: Array
+			  on 'q', 'quiet',     'not show test details',                argument: :optional
+			  on 'f', 'from',      'test number to start from',            argument: :optional
+			  on 't', 'to',        'test number to stop to',               argument: :optional
+			  on 'l', 'load',      'An optional password',                 argument: :optional
+			  on 'd', 'dryrun',    'not run test but show just structure', argument: :optional
+			  on 'r', 'reporters', 'reporters to use',                     argument: :optional, as: Array
+			end
+			opt.to_hash
+		end
+
+		def self.start
+			options = parse_options
+			Runner.new(options).run(ARGV)
 		end
 	end
 end
