89957e6c6a2f913ab536a9fa3d6fb8e30113b43b
tily <tidnlyam@gmail.com>
2014-11-05 10:24:50 +0900

    add Barthes::Client::HTTParty stub

---
+++ lib/barthes/client/httparty.rb
@@ -0,0 +1,17 @@
+module Barthes
+	module Client
+		class HTTParty
+			def initialize(env)
+			end
+
+			def action(params)
+			end
+
+			def compare(response, expectation)
+			end
+
+			def extract(config, response)
+			end
+		end
+	end
+end
