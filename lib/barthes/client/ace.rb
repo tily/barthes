1cdc5654e0e393dbcc0f3fcbb115334fc3aa88c3
tily <tidnlyam@gmail.com>
2014-11-19 23:44:33 +0900

    fix bug

--- lib/barthes/client/ace.rb
+++ lib/barthes/client/ace.rb
@@ -1,4 +1,5 @@
 require 'ace-client'
+require 'barthes/client/httparty'
 require 'active_support/core_ext/hash/keys'
 
 class AceClient::Base
@@ -9,46 +10,42 @@ class AceClient::Base
 	}
 end
 
-module Barthes
-	module Client
-		class Ace
-			OPTION_KEYS = %w(
-				authorization_key
-				signature_method
-				authorization_prefix
-				service
-				region
-				host
-				nonce
-				access_key_id_key
-				path
-				use_ssl
-				endpoint
-				response_format
-				http_method
-				access_key_id
-				secret_access_key
-				timeout
-			)
+class Barthes::Client::Ace < Barthes::Client::HTTParty
+	OPTION_KEYS = %w(
+		authorization_key
+		signature_method
+		authorization_prefix
+		service
+		region
+		host
+		nonce
+		access_key_id_key
+		path
+		use_ssl
+		endpoint
+		response_format
+		http_method
+		access_key_id
+		secret_access_key
+		timeout
+	)
 
-			def initialize(env)
-				# TODO: validate existence of required options
-				@options = env.slice(*OPTION_KEYS).symbolize_keys
-				if env['client'] && env['client']['user']
-					@user = env['client']['user']
-					@options['access_key_id'] = env['client']['access_key_id']
-					@options['secret_access_key'] = env['client']['secret_access_key']
-					@options.update(env['client'].slice(*OPTION_KEYS).symbolize_keys)
-				end
-			end
-
-			def action(params)
-				params = params.dup
-				action = params.delete('Action')
-				params ||= {}
-				response = @client.action(action, params)
-				response
-			end
+	def initialize(env)
+		# TODO: validate existence of required options
+		@options = env.slice(*OPTION_KEYS).symbolize_keys
+		if env['client'] && env['client']['user']
+			@user = env['client']['user']
+			@options['access_key_id'] = env['client']['access_key_id']
+			@options['secret_access_key'] = env['client']['secret_access_key']
+			@options.update(env['client'].slice(*OPTION_KEYS).symbolize_keys)
 		end
+	end
+
+	def action(params)
+		params = params.dup
+		action = params.delete('Action')
+		params ||= {}
+		response = @client.action(action, params)
+		response
 	end
 end
