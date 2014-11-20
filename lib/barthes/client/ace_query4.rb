d21ab6a117d8ab8251edba8e1570b1e20c2712c9
tily <tidnlyam@gmail.com>
2014-11-20 10:46:21 +0900

    fix bug

--- lib/barthes/client/ace_query4.rb
+++ lib/barthes/client/ace_query4.rb
@@ -1,7 +1,7 @@
 class Barthes::Client::AceQuery4 < Barthes::Client::Ace
         def initialize(env)
 		super(env)
-		@client = AceClient::Query3.new(@options)
+		@client = AceClient::Query4.new(@options)
         end
 end
 
